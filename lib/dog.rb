69a79d47f8e1fd06fe1ea12588ffc9f0add9ee83
Jorge Venegas <brangelina01@gmail.com>
2020-07-18 02:28:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
-class Dog 
-  def bark
-    puts "woof"
-  end 
-end 
-
-dan = Dog.new 
-peter = Dog.new 
-snoop = Dog.new 
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
